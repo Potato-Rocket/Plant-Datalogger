31d1f0214f7c288f992dd9bf5ae9d7749fad7469
Oscar Stomberg <oscar@stomberg.us>
2025-02-02 19:54:36 -0500

    Set up indicator light

--- Pico W Code/main.c
+++ Pico W Code/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include "hardware/rtc.h"
 #include "pico/stdlib.h"
+#include "pico/util/datetime.h"
 
-int main()
-{
+#define LED_PIN 22
+
+int main() {
     stdio_init_all();
+    
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_set_drive_strength(LED_PIN, GPIO_DRIVE_STRENGTH_12MA);
 
     while (true) {
-        printf("Hello, world!\n");
+        printf("LED on!\n");
+        gpio_put(LED_PIN, true);
+        sleep_ms(1000);
+
+        printf("LED off!\n");
+        gpio_put(LED_PIN, false);
         sleep_ms(1000);
     }
 }
-        
