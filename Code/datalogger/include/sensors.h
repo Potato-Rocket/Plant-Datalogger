6b079b85d412186c81bfa6b9b6636526674e4013
Oscar Stomberg <oscar@stomberg.us>
2025-04-11 15:09:42 -0400

    Implemented example code. Does not work.

--- Code/datalogger/include/sensors.h
+++ Code/datalogger/include/sensors.h
@@ -2,4 +2,11 @@
 
 #include "pico/stdlib.h"
 
+typedef struct {
+    float humidity;
+    float temp_celsius;
+} dht_reading_t;
+
 bool init_sensors(void);
+
+void read_from_dht(dht_reading_t *result);
--- Code/datalogger/src/main.c
+++ Code/datalogger/src/main.c
@@ -31,15 +31,19 @@ int main() {
 
     // set up variables for the update loop
     uint64_t prev_loop = 0;
+    dht_reading_t temp_humidity;
 
     while (true) {
         if (should_check_wifi()) {
             wifi_check_reconnect();
         }
-        
+
         if (time_us_64() - prev_loop > LOOP_DELAY_US) {
             rtc_print();
             prev_loop = time_us_64();
+
+            printf("Temperature: %.1fC  Humidity: %.1f%%\n",
+            temp_humidity.temp_celsius, temp_humidity.humidity);
         }
     }
 
