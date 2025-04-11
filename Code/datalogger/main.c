6b8ea9d201c8cbf16278a7113cad92ca24c308f6
Oscar Stomberg <oscar@stomberg.us>
2025-04-10 21:01:28 -0400

    Remove code to toggle light, as well as timers/alarms, reinforces startup sequence

--- Code/datalogger/main.c
+++ Code/datalogger/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "pico/util/datetime.h"
 #include "hardware/rtc.h"
@@ -6,100 +7,108 @@
 
 #define LED_PIN 22
 
+#define LOOP_DELAY_US 1000000
+#define INIT_WAIT_US 10000000
+#define INIT_WAIT_MS 10000
+
 // MAC address is 28:CD:C1:0E:C6:5B
 #define ssid "WPI-PSK"
 #define pass "photosynthesize"
 
-typedef struct {
-    int count;
-    datetime_t now;
-    char datetime_buf[256];
-    char* datetime_str;
-} userdata_t;
+bool init_modules();
 
-int init_modules();
-
-int init_wifi();
-
-bool cb_update_time(struct repeating_timer *t);
-
-int64_t cb_led_off(__unused alarm_id_t id, __unused void *userdata);
+bool init_wifi();
 
 int main() {
-    userdata_t data = {
-        .count = 0,
-        .now = {
-            .year  = 2025,
-            .month = 02,
-            .day   = 02,
-            .dotw  = 0,
-            .hour  = 23,
-            .min   = 45,
-            .sec   = 00
-        },
-        .datetime_str = &data.datetime_buf[0]
-    };
+    // set up sensors, output devices, etc.
+    if (!init_modules()) {
+        return -1;
+    }
 
-    init_modules();
-
-    int init = init_wifi();
-    if (init != 0) {
-        return init;
+    // try to connect to wifi
+    if (!init_wifi()) {
+        return -1;
     }
     
-    rtc_set_datetime(&data.now);
+    // create and set default date and time
+    datetime_t now;
 
-    struct repeating_timer update_timer;
-    add_repeating_timer_ms(-1000, cb_update_time, &data, &update_timer);
+    // buffer and pointer for storing the datetime string
+    char datetime_buf[256];
+    char* datetime_str = &datetime_buf[0];
+
+    // set up variables for the update loop
+    uint64_t prev_time = 0;
+    uint32_t count = 0;
 
     while (true) {
-        tight_loop_contents();
+        if (time_us_64() - prev_time > LOOP_DELAY_US) {
+            // update and print the current datetime
+            rtc_get_datetime(&now);
+            datetime_to_str(datetime_str, sizeof(datetime_buf), &now);
+            printf("%ds since startup... %s\n", count, datetime_str);
+            // update loop variables
+            count++;
+            prev_time = time_us_64();
+        }
     }
+
 }
 
-int init_modules() {
+bool init_modules() {
+    // wait up to ten seconds for the serial port to open
     stdio_init_all();
+    if (uart_is_readable_within_us(uart0, INIT_WAIT_US)) {
+        printf("Serial connection success!\n");
+    }
 
+    // set up indicator light
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     gpio_set_drive_strength(LED_PIN, GPIO_DRIVE_STRENGTH_12MA);
     
+    // initialize the RTC
     rtc_init();
-    return 0;
-}
-
-int init_wifi() {
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed!\n");
-        return -1;
+    datetime_t t = {
+        .year  = 2025,
+        .month = 1,
+        .day   = 1,
+        .dotw  = 3,
+        .hour  = 0,
+        .min   = 0,
+        .sec   = 0
+    };
+    rtc_set_datetime(&t);
+    uint32_t start_time = time_us_32();
+    while (!rtc_running()) {
+        if (time_us_32() - start_time > INIT_WAIT_US) {
+            printf("RTC init failed!\n");
+            return false;
+        }
     }
-    printf("Wi-Fi init success!\n");
+    printf("RTC init success!\n");
 
-    cyw43_arch_enable_sta_mode();
-    
-    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass,
-        CYW43_AUTH_WPA2_AES_PSK, 10000)) {
-        printf("Wifi connection failed!\n");
-        return -1;
-    }
-    printf("Wifi connection success!\n");
-    
-    return 0;
-}
-
-bool cb_update_time(struct repeating_timer *t) {
-    userdata_t* data = (userdata_t*)t->user_data;
-    rtc_get_datetime(&data->now);
-
-    gpio_put(LED_PIN, 1);
-    datetime_to_str(data->datetime_str, sizeof(data->datetime_buf), &data->now);
-    printf("%ds since startup... %s\n", data->count, data->datetime_str);
-    data->count++;
-    add_alarm_in_ms(100, cb_led_off, NULL, true);
     return true;
 }
 
-int64_t cb_led_off(__unused alarm_id_t id, __unused void *userdata) {
-    gpio_put(LED_PIN, 0);
-    return 0;
+bool init_wifi() {
+    // initialize the WiFi chip
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed!\n");
+        return false;
+    }
+    printf("Wi-Fi init success!\n");
+
+    // enable station mode
+    cyw43_arch_enable_sta_mode();
+
+    // connect to the network
+    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass,
+        CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+        printf("Wifi connection failed!\n");
+        return false;
+    }
+    printf("Wifi connection success!\n");
+
+    return true;
 }
